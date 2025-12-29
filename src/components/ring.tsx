0fb55737d1915410ecdf21f2a71b61b39f520dad
Ray Samanta <raysamanta083@gmail.com>
2025-12-29 22:05:19 +0000

    Try fixing this error: `Runtime TypeError: Cannot read properties of und

--- src/components/ring.tsx
+++ src/components/ring.tsx
@@ -3,7 +3,7 @@
 import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
-import { useRef } from 'react'
+import { useRef, Suspense } from 'react'
 
 function Diamond(props: JSX.IntrinsicElements['group']) {
   const ref = useRef<THREE.Group>(null!)
@@ -23,6 +23,7 @@ function Diamond(props: JSX.IntrinsicElements['group']) {
   });
 
   useFrame((state) => {
+    if (!ref.current) return;
     const t = state.clock.getElapsedTime()
     ref.current.rotation.x = -Math.PI / 1.75 + Math.cos(t / 4) / 8
     ref.current.rotation.y = Math.sin(t / 2) / 1
@@ -37,29 +38,7 @@ function Diamond(props: JSX.IntrinsicElements['group']) {
   )
 }
 
-function Gem(props: JSX.IntrinsicElements['group']) {
-  const ref = useRef<THREE.Group>(null!)
-  const { nodes } = useGLTF('https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/gem/model.gltf') as any;
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime()
-    ref.current.rotation.x = -Math.PI / 1.75 + Math.cos(t / 4) / 8
-    ref.current.rotation.y = Math.sin(t / 2) / 1
-    ref.current.rotation.z = (1 + Math.sin(t / 1.5)) / 20
-    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
-  })
-  return (
-    <group ref={ref} {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.gem.geometry}
-        material={nodes.gem.material}
-      />
-    </group>
-  )
-}
-
-export function Ring() {
+function Model() {
   const { nodes } = useGLTF('https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/ring/model.gltf') as any;
 
   // Gold material
@@ -70,16 +49,24 @@ export function Ring() {
   });
 
   return (
+    <group position={[0, -5, 0]}>
+        <mesh geometry={nodes.ring.geometry} material={goldMaterial} receiveShadow castShadow />
+        <Diamond position={[0, 17, 0]} scale={[8, 8, 8]} />
+    </group>
+  )
+}
+
+export function Ring() {
+  return (
     <Canvas
       style={{ touchAction: 'none' }}
       camera={{ position: [0, 0, 100], fov: 25 }}
     >
       <ambientLight intensity={1} />
       <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-      <group position={[0, -5, 0]}>
-        <mesh geometry={nodes.ring.geometry} material={goldMaterial} receiveShadow castShadow />
-        <Diamond position={[0, 17, 0]} scale={[8, 8, 8]} />
-      </group>
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
       <OrbitControls enableZoom={false} enablePan={false} />
     </Canvas>
   )
