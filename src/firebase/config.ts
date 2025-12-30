f7202ce2b70859193362808b38bbafbd257a1755
Ray Samanta <raysamanta083@gmail.com>
2025-12-30 17:42:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4385898867-8572f",
+  "appId": "1:80771448384:web:2d91f61ff3742e3b8a69a5",
+  "apiKey": "AIzaSyDZSUpkpWEqbmM-Pt0qLtKKKBROzJZcf7A",
+  "authDomain": "studio-4385898867-8572f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "80771448384"
+};
